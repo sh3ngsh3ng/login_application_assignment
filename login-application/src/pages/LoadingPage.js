bbd4065b047e8cccb5789aa67e6090e90e1793ad
sh3ngsh3ng <yshenglim13@gmail.com>
2022-02-09 19:00:07 +0000

    loading page set up

--- login-application/src/pages/LoadingPage.js
+++ login-application/src/pages/LoadingPage.js
@@ -1,13 +1,31 @@
-import React from 'react'
-
+import React, {useState, useEffect} from 'react'
+import {useHistory} from "react-router-dom"
+import axios from 'axios'
 
 export default function LoadingPage() {
+    const [apiLoaded, setApiLoaded] = useState(false)
+    let history = useHistory()
 
+    let API_URL = "https://3000-sh3ngsh3ng-loginapplicat-h17zq07u389.ws-us31.gitpod.io/"
+
+    useEffect(() => {
+        async function checkApiLive() {
+            let response = await axios.get(API_URL)
+            setApiLoaded(response.data.live)
+        }
+        checkApiLive()
+    },[])
+
+    useEffect(() => {
+        setTimeout(function() {
+            history.push("/login")
+        }, 3000)
+    }, [apiLoaded])
 
 
     return (
         <React.Fragment>
-            Page is loading....
+            Put loading image/spinner here
         </React.Fragment>
     )
 }
