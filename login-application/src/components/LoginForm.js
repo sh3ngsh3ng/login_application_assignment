68f4ef94e65f4087e2a6c29802e9ccf0d62cd19d
sh3ngsh3ng <yshenglim13@gmail.com>
2022-02-10 19:06:33 +0000

    s

--- login-application/src/components/LoginForm.js
+++ login-application/src/components/LoginForm.js
@@ -5,6 +5,7 @@ import "./LoginForm.css"
 import axios from "axios"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import language from "./language.json"
 
 export default function LoginForm() {
 
@@ -16,6 +17,8 @@ export default function LoginForm() {
         "username": "",
         "password": ""
     })
+
+    const [lang, setLang] = useState("en")
 
     const onUpdateFormField = (e) => {
         setForm({
@@ -66,21 +69,26 @@ export default function LoginForm() {
                 <img class="form-brand-logo" src={logo}/>
             </div>
             <div id="login-username-div">
-                <input id="userIdInput" placeholder="Enter Username" name="username" className="form-control" value={form.username} onChange={onUpdateFormField} />
+                <input id="userIdInput" placeholder={language[lang].loginUsernameField} name="username" className="form-control" value={form.username} onChange={onUpdateFormField} />
             </div>
             <div id="login-password-div">
-                <input id="passwordInput" type="password" placeholder="Password" name="password" className="form-control" value={form.password} onChange={onUpdateFormField} />
+                <input id="passwordInput" type="password" placeholder={language[lang].loginPasswordField} name="password" className="form-control" value={form.password} onChange={onUpdateFormField} />
             </div>
 
             <div id="login-btn-div">
-                <btn id="login-btn" className="btn btn-success" onClick={() => submitForm(form)}>Login</btn>
+                <btn id="login-btn" className="btn btn-success" onClick={() => submitForm(form)}>{language[lang].loginButtonText}</btn>
             </div>
             <div id="sign-up-prompt-div">
-                <span id="sign-up-prompt">Don't have an account?</span>
+                <span id="sign-up-prompt">{language[lang].signUpPrompt}</span>
             </div>
             
             <div id="sign-up-btn-div">
-                <btn id="sign-up-btn"className="btn btn-secondary" onClick={() => history.push("/sign-up")}>Sign Up</btn>
+                <btn id="sign-up-btn"className="btn btn-secondary" onClick={() => history.push("/sign-up")}>{language[lang].signUpButtonText}</btn>
+            </div>
+            <div style={{"display": "flex", "justifyContent": "center", "margin-top": "10px"}}>
+                <span className="language-text" role="button" title="en" onClick={(evt) => setLang(evt.target.title)}>{language["en"].selectLang}</span>
+                <span>&nbsp;/&nbsp; </span>
+                <span className="language-text" role="button" title="chi" onClick={(evt) => setLang(evt.target.title)}>{language["chi"].selectLang}</span>
             </div>
             </div>
         </div>
